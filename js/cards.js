49b47facfee541617ee6a10938774bd88d74beb7
Alice Ji <rji3@illinois.edu>
2025-10-09 22:45:46 -0500

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,4 +1,5 @@
 const cards = [
+  // ======== COMMON (24) ========
   {
     name: "Lily Cat",
     power: 8,
@@ -35,6 +36,116 @@ const cards = [
     rarity: "common",
     img: "https://alice-ji.github.io/Gacha/assets/toadstooltwins.png",
   },
+  {
+    name: "Baobab Guardian",
+    power: 9,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/baobabguardian.png",
+  },
+  {
+    name: "Mushroom Boy",
+    power: 9,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/mushroomboy.png",
+  },
+  {
+    name: "Birch Brother",
+    power: 10,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/birchbrother.png",
+  },
+  {
+    name: "Bloom Spirit",
+    power: 8,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/bloomspirit.png",
+  },
+  {
+    name: "Tulip Tortoise",
+    power: 9,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/tuliptortoise.png",
+  },
+  {
+    name: "Minty Moth",
+    power: 8,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/mintymoth.png",
+  },
+  {
+    name: "Peony Pup",
+    power: 8,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/peonypup.png",
+  },
+  {
+    name: "Pitcher Sprite",
+    power: 8,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/pitchersprite.png",
+  },
+  {
+    name: "Creeper Pixie",
+    power: 10,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/creeperpixie.png",
+  },
+  {
+    name: "Dreamroot Sloth",
+    power: 8,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/dreamrootsloth.png",
+  },
+  {
+    name: "Reed Runner",
+    power: 8,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/reedrunner.png",
+  },
+  {
+    name: "Snapdragon Sentry",
+    power: 9,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/snapdragonsentry.png",
+  },
+  {
+    name: "Cactus Kid",
+    power: 9,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/cactuskid.png",
+  },
+  {
+    name: "Rootfather Elm",
+    power: 9,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/rootfatherelm.png",
+  },
+  {
+    name: "Thistle Rogue",
+    power: 10,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/thistlerogue.png",
+  },
+  {
+    name: "Mandrake Snout",
+    power: 11,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/mandrakesnout.png",
+  },
+  {
+    name: "Aconite Chinchilla",
+    power: 9,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/aconitechinchilla.png",
+  },
+  {
+    name: "Dahlia Kitten",
+    power: 10,
+    rarity: "common",
+    img: "https://alice-ji.github.io/Gacha/assets/dhaliakitten.png",
+  },
+
+  // ======== RARE (12) ========
   {
     name: "Dandelion Puff",
     power: 12,
@@ -84,6 +195,32 @@ const cards = [
     img: "https://alice-ji.github.io/Gacha/assets/bramblebear.png",
   },
   {
+    name: "Volcan Lily Warrior",
+    power: 14,
+    rarity: "rare",
+    img: "https://alice-ji.github.io/Gacha/assets/volcanlilywarrior.png",
+  },
+  {
+    name: "Heliconia Harlequin",
+    power: 13,
+    rarity: "rare",
+    img: "https://alice-ji.github.io/Gacha/assets/heliconiaharlequin.png",
+  },
+  {
+    name: "Agave Alchemist",
+    power: 15,
+    rarity: "rare",
+    img: "https://alice-ji.github.io/Gacha/assets/agavealchemist.png",
+  },
+  {
+    name: "Honeysuckle Herald",
+    power: 14,
+    rarity: "rare",
+    img: "https://alice-ji.github.io/Gacha/assets/honeysuckleherald.png",
+  },
+
+  // ======== LEGENDARY (4) ========
+  {
     name: "Lotus Empress",
     power: 25,
     rarity: "legendary",
@@ -101,8 +238,15 @@ const cards = [
     rarity: "legendary",
     img: "https://alice-ji.github.io/Gacha/assets/nightshadeoracle.png",
   },
+  {
+    name: "Thorn Regent",
+    power: 25,
+    rarity: "legendary",
+    img: "https://alice-ji.github.io/Gacha/assets/thornregent.png",
+  },
 ];
 
+// ======== Pull odds  ========
 const rarityOdds = {
   common: 0.7,
   rare: 0.25,
